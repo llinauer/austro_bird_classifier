8728b673d0f91711e2877f7b235bbbc9f728f77c
llinauer <lukaslinauer1@gmail.com>
2023-03-18 18:21:20 +0100

    Add create_mini_ds.py

---
+++ data/create_mini_ds.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+"""
+create_mini_ds.py
+
+Create a mini version of the train and test datasets.
+
+"""
+
+__author__ = 'Lukas Linauer'
+__email__ = 'l.linauer@gmx.at'
+
+import os
+from pathlib import Path
+
